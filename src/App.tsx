dc53a37ef76565575b23c3d18684e6e163b9ff57
TranQuocToan1996 <tranquoctoan.ce@gmail.com>
2025-11-16 17:58:59 +0700

    Feat: staleTime react query

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,17 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { HomePage } from './pages/HomePage';
 import './index.css';
 
+
+const STALE_TIME_MINUTES = 5;
+const GC_TIME_MINUTES = 10;
+
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
       refetchOnWindowFocus: false,
       retry: 1,
+      staleTime: STALE_TIME_MINUTES * 60 * 1000,
+      gcTime: GC_TIME_MINUTES * 60 * 1000,
     },
   },
 });
